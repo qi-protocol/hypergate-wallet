ea8fff6d2f7fadd92c0f73f0fb7fcdc0a2eb90b9
FudgyDRS <yetimonsters@gmail.com>
2023-08-31 03:26:10 -0700

    rewriting l1l1 msg

--- script/L1L1Message.s.sol
+++ script/L1L1Message.s.sol
@@ -2,45 +2,22 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Script.sol";
+import "forge-std/Test.sol";
+import "forge-std/StdCheats.sol";
+import "forge-std/console.sol";
 
-// needs to recieve a message from ccip (UserOp, PaymasterAddress)
-// then release locked funds
-// deserialize PaymasterAndData (paymaster, chainid, target, owner, amount)
-// chainid == block.chainid
-// validateSignature == owner
-// transfer amount to paymaster 
-
-// contracts
-// TestPaymaster(IEntryPoint(owner)) L1
-//      call validateSignature
-//      call validatePaymasterUserOp
-/*
-        uint256 gas = verificationGasLimit - gasUsedByValidateAccountPrepayment;
-            address paymaster = mUserOp.paymaster;
-            DepositInfo storage paymasterInfo = deposits[paymaster];
-            uint256 deposit = paymasterInfo.deposit;
-            if (deposit < requiredPreFund) {
-                revert FailedOp(opIndex, "AA31 paymaster deposit too low");
-            }
-            paymasterInfo.deposit = uint112(deposit - requiredPreFund);
-            try IPaymaster(paymaster).validatePaymasterUserOp{gas : gas}(op, opInfo.userOpHash, requiredPreFund) returns (bytes memory _context, uint256 _validationData){
-                context = _context;
-                validationData = _validationData;
-            }
-    the above implies verificationGasLimit - gasUsedByValidateAccountPrepayment = gas
-    is the gas that will be used for the execution of the crosschain call
-
-    Execution conditions:
-    - paymasterAndData >= 20 bytes
-    - enough gas
-    - sufficent deposited ETH on the paymaster
-        checked via _getValidationData(paymasterValidationData)
-    - 
-*/
-
-// TestEscrow
-// TestEntryPoint
-// PrintUserOp
-// Have TestEscrow print out userop
-// Have TestEscrow call TestEntrypoint
-//      which will call contract PrintUserOp
+contract TestL1L1Message is Test{
+    // broadcast
+    // just deploy escrow on origin chain
+    // directly call cciprouter from execution chain
+    // need to have dumbed dowed call
+    function testDirectCall() public {
+        vm.chainId(chainId_1);
+        string memory key = vm.readFile(".secret");
+        bytes32 key_bytes = vm.parseBytes32(key);
+        uint256 privateKey;
+        assembly {
+            privateKey := key_bytes
+        }
+    }
+}
