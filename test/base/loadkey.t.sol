93d376658e4bdc6fd11e52469a89c1d6d7100d8d
FudgyDRS <yetimonsters@gmail.com>
2023-09-25 18:41:25 -0700

    removed sruff

--- test/base/loadkey.t.sol
+++ test/base/loadkey.t.sol
@@ -3,27 +3,6 @@ pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
 
-//import {IMailbox, IIGP, TestPaymaster, IEntryPoint} from "flat/TestPaymaster2_f.sol";
-//import "contracts/interfaces/ITestEscrow.sol";
-
-import {IEntryPoint, EntryPoint, IAccount} from "@4337/core/EntryPoint.sol";
-import {SimpleAccount, SimpleAccountFactory} from "@4337/samples/SimpleAccountFactory.sol";
-import {TestPaymaster} from "contracts/TestPaymaster.sol";
-import {TestEscrow} from "contracts/TestEscrow.sol";
-import "contracts/interfaces/ITestEscrow.sol";
-
-/**
-What I need
-- Paymaster needs to be deployed
-- The paymaster need to have BOTH deposited and staked funds in the EntryPoint
-- Test is paymaster works locally with normal transactions
-- Escrow test already works
-- Test Hyperlane live transactions (easier since hyperlane Mumbai/sepolia doesn’t need payment)
-- If all works, make it reproducible with instructions
-- Make a video of stepping though the process
-- Post video to my YouTube and share it (less than 3 mins)
- */
-
  contract LoadKey is Test {
 
     address eoaAddress;
